1077ba9953ab0a7d0999cd298bc1483e96097ac5
Drygala Marina <drygala@haas001.ds-a4-r02.cct.rcp.epfl.ch>
2026-05-01 14:17:42 +0200

    cleaning up warpslab

---
+++ matrix_multiplication/clean_restart/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef SUBTILE
+#define SUBTILE 64
+#endif
+
+#ifndef SUB
+#define SUB 4
+#endif
+
+#include <string>
+
+struct Config{
+    std::string kernel_type = "warpslab";
+
+};
